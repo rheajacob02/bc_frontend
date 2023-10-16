41b163e4357507c572f42e25cd82831a5de1b5be
rheajacob02 <21rhea11jacob02@gmail.com>
2023-10-16 14:03:26 +0530

    tried to make libp2p work

--- src/pages/admin.js
+++ src/pages/admin.js
@@ -3,8 +3,36 @@ import { Buffer } from "buffer";
 import { createHelia } from "helia";
 // import { dagCbor } from "@helia/dag-cbor";
 import { unixfs } from "@helia/unixfs";
+import { createLibp2p } from "libp2p";
+import { identifyService } from "libp2p/identify";
+import { noise } from "@chainsafe/libp2p-noise";
+import { yamux } from "@chainsafe/libp2p-yamux";
+import { bootstrap } from "@libp2p/bootstrap";
+import { tcp } from "@libp2p/tcp";
 
-const helia = await createHelia();
+const libp2p = await createLibp2p({
+  addresses: {
+    listen: ["/ip4/127.0.0.1/tcp/5001"],
+  },
+  transports: [tcp()],
+  connectionEncryption: [noise()],
+  streamMuxers: [yamux()],
+  peerDiscovery: [
+    bootstrap({
+      list: [
+        "/dnsaddr/bootstrap.libp2p.io/p2p/QmNnooDu7bfjPFoTZYxMNLWUQJyrVwtbZg5gBMjTezGAJN",
+        "/dnsaddr/bootstrap.libp2p.io/p2p/QmQCU2EcMqAqQPR2i9bChDtGNJchTbq5TbXJJ16u19uLTa",
+        "/dnsaddr/bootstrap.libp2p.io/p2p/QmbLHAnMoJPWSCR5Zhtx6BHJX9KiKNN6tpvbUcqanj75Nb",
+        "/dnsaddr/bootstrap.libp2p.io/p2p/QmcZf59bWwK5XFi76CZX8cbJ4BhTzzA3gU1ZjYZcYW3dwt",
+      ],
+    }),
+  ],
+  services: {
+    identify: identifyService(),
+  },
+});
+
+const helia = await createHelia(libp2p);
 // const c = dagCbor(helia)
 const fs = unixfs(helia);
 
