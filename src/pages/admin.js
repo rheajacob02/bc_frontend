734d926d4050fdc6483d7ad8f0aa7c6dc8c8b92a
rheajacob02 <21rhea11jacob02@gmail.com>
2023-10-19 14:17:15 +0530

    testing with ec2 ipfs

--- src/pages/admin.js
+++ src/pages/admin.js
@@ -1,13 +1,13 @@
 import { React, useState } from "react";
 import { Buffer } from "buffer";
 import { create } from "kubo-rpc-client";
+import axios from "axios"; // Install this using "npm install axios"
 
-const client = create("/ip4/127.0.0.1/tcp/5001");
+const client = create("/ip4/65.0.30.242/tcp/5001");
 
 export const Admin = () => {
   const [buffer, setBuffer] = useState(null);
   const [name, setName] = useState("");
-  // const [cid, setCID] = useState("")
 
   const handleChange = (event) => {
     event.stopPropagation();
@@ -24,11 +24,35 @@ export const Admin = () => {
     };
   };
 
+  const computeHash = async (buf) => {
+    // Compute the SHA-256 hash of the buffer
+    const digest = await window.crypto.subtle.digest("SHA-256", buf);
+    return Array.from(new Uint8Array(digest))
+      .map((b) => b.toString(16).padStart(2, "0"))
+      .join("");
+  };
+
   const handleSubmit = async (event) => {
     event.preventDefault();
     const u8 = new Uint8Array(Buffer.from(buffer));
     const { cid } = await client.add(u8);
     client.files.cp(`/ipfs/${cid}`, `/${name}`);
+
+    const rawHash = await computeHash(buffer);
+    console.log(rawHash);
+    console.log(cid.toString());
+
+    try {
+      // Send the rawHash and CID to your backend
+      await axios.post("http://127.0.0.1:3001/addDocument", {
+        rawHash: `0x${rawHash}`,
+        cid: cid.toString(),
+      });
+      console.log("Data sent successfully");
+    } catch (error) {
+      console.error("Error sending data to backend:", error);
+    }
+
     document.getElementById("file_input").value = "";
   };
 
