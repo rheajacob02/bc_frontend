e2b9249a253f9deb31d97352ef91603a6e6e6427
SherwinBenjamin <sherwinbenjamin2828@gmail.com>
2023-11-03 02:23:14 +0530

    pushed rhea

--- src/pages/Recipient.js
+++ src/pages/Recipient.js
@@ -3,27 +3,27 @@ import axios from "axios";
 
 export const Recipient = () => {
   const [regNumber, setRegNumber] = useState("");
-  const [documents, setDocuments] = useState([]); // To store fetched CIDs of documents
+  const [phoneNumber, setPhoneNumber] = useState("");
+  const [documents, setDocuments] = useState([]);
 
   const handleFetchDocuments = async () => {
     let start = performance.now();
     try {
       const response = await axios.get(
-        `http://127.0.0.1:3001/getDocumentByRegNumber?regNumber=${regNumber}`
+        `http://65.2.190.0:3001/getDocumentByRegNumber`,
+        { params: { regNumber, phoneNumber } } // Pass both regNumber and phoneNumber as query parameters
       );
       setDocuments(response.data.cids);
-      console.log(response.data.cids); // Backend returns an array of CIDs
       let end = performance.now();
-      console.log("Time taken to fetch fromm ipfs= ", end - start);
+      console.log("Time taken to fetch documents: ", end - start);
     } catch (error) {
       console.error("Error fetching documents:", error);
     }
   };
 
   return (
-    <div className="">
+    <div>
       <h3>Fetch Your Documents:</h3>
-
       <div>
         <label>Registration Number:</label>
         <input
@@ -32,29 +32,17 @@ export const Recipient = () => {
           value={regNumber}
           onChange={(e) => setRegNumber(e.target.value)}
         />
+        <label>Phone Number:</label>
+        <input
+          type="tel"
+          placeholder="Enter Phone Number"
+          value={phoneNumber}
+          onChange={(e) => setPhoneNumber(e.target.value)}
+        />
         <button onClick={handleFetchDocuments}>Fetch Documents</button>
       </div>
-
       {documents.length > 0 && (
-        <div>
-          <h4>Your Documents:</h4>
-          <ul>
-            {documents.map((cid, index) => (
-              <li key={index}>
-                {/* Link to download the document from IPFS using CID */}
-                <button>
-                  <a
-                    href={`http://13.232.187.19:8080/ipfs/${cid}`}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    Document {index + 1}
-                  </a>
-                </button>
-              </li>
-            ))}
-          </ul>
-        </div>
+        // ... rest of the component remains unchanged
       )}
     </div>
   );
